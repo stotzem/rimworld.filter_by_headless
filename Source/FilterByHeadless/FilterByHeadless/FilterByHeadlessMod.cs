938eceef5f760dd4b9f263956850636338d8d358
Oliver Stotzem <5693011+stotzem@users.noreply.github.com>
2025-06-14 11:33:24 +0200

    Update to 1.6

---
+++ Source/FilterByHeadless/FilterByHeadless/FilterByHeadlessMod.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using RimWorld;
+using Verse;
+
+namespace FilterByHeadless
+{
+    public class SpecialThingFilterWorkerFilterByHeaded : SpecialThingFilterWorker
+    {
+        public override bool Matches(Thing t)
+        {
+            var corpse = t as Corpse;
+            return corpse != null && corpse.InnerPawn.health.hediffSet.GetNotMissingParts()
+                .Any(p => p.def == BodyPartDefOf.Head);
+        }
+    }
+
+    public class SpecialThingFilterWorkerFilterByHeadless : SpecialThingFilterWorker
+    {
+        public override bool Matches(Thing t)
+        {
+            var corpse = t as Corpse;
+            return corpse == null || corpse.InnerPawn.health.hediffSet.GetNotMissingParts()
+                .All(p => p.def != BodyPartDefOf.Head);
+        }
+    }
+}
--- Source/FilterByHeadless/FilterByHeadless/Properties/AssemblyInfo.cs
+++ Source/FilterByHeadless/FilterByHeadless/Properties/AssemblyInfo.cs
@@ -1,15 +1,14 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("[1001]FilterByHeadless")]
+[assembly: AssemblyTitle("FilterByHeadless")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("[1001]FilterByHeadless")]
+[assembly: AssemblyProduct("FilterByHeadless")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
